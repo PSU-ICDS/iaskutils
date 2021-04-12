cc33b0e11e94f9e57945a5c221708388e2eb6f19
NucciTheBoss <jcn23@psu.edu>
2021-04-12 12:02:38 -0400

    Update module file

--- share/modules/1.2.lua
+++ share/modules/1.2.lua
@@ -1,7 +1,7 @@
 help([[iaskutils is a collection of various python scripts and tools used 
 by the Penn State ICDS i-ASK helpdesk to help the users of the Roar supercomputer.
 
-The iaskutils collection currently consists of the following scripts"
+The iaskutils collection currently consists of the following scripts:
 
 - collector: A simple script to collect information about your environment.
 
@@ -17,12 +17,12 @@ For help using these commands, simply refer to the man pages or please contact t
 at iask@ics.psu.edu for assistance.]])
 
 whatis("Description: iaskutils is a collection of various python scripts and tools used by the Penn State ICDS i-ASK helpdesk to help the users of the Roar supercomputer.")
-whatis("collector version: 1.1")
+whatis("collector version: 1.2")
 whatis("gathero version: 1.2")
-whatis("relinkworkscratch version: 1.1")
-whatis("setupcomsolsymlink version: 1.1")
-whatis("setupcondasymlink version: 1.1")
-whatis("GitHub repository: https://github.com/NucciTheBoss/iaskutils")
+whatis("relinkworkscratch version: 1.2")
+whatis("setupcomsolsymlink version: 1.2")
+whatis("setupcondasymlink version: 1.2")
+whatis("GitHub repository: https://github.com/ICDS-Roar/iaskutils")
 whatis("Author: Jason C. Nucciarone (jason.nucciarone@gmail.com, jcn23@psu.edu")
 
 prepend_path("PATH", "/gpfs/group/dml129/default/sw7/iaskutils/bin")
