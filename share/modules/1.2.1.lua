02ca78bfb84c7f828d368deec127e8b82c61f1c1
NucciTheBoss <jcn23@psu.edu>
2021-04-17 15:42:01 -0400

    Update versions and add custom python

--- share/modules/1.2.1.lua
+++ share/modules/1.2.1.lua
@@ -17,13 +17,25 @@ For help using these commands, simply refer to the man pages or please contact t
 at iask@ics.psu.edu for assistance.]])
 
 whatis("Description: iaskutils is a collection of various python scripts and tools used by the Penn State ICDS i-ASK helpdesk to help the users of the Roar supercomputer.")
-whatis("collector version: 1.2")
-whatis("gathero version: 1.2")
-whatis("relinkworkscratch version: 1.2")
-whatis("setupcomsolsymlink version: 1.2")
-whatis("setupcondasymlink version: 1.2")
+whatis("collector version: 1.2.1")
+whatis("gathero version: 1.2.1")
+whatis("relinkworkscratch version: 1.2.1")
+whatis("setupcomsolsymlink version: 1.2.1")
+whatis("setupcondasymlink version: 1.2.1")
 whatis("GitHub repository: https://github.com/ICDS-Roar/iaskutils")
 whatis("Author: Jason C. Nucciarone (jason.nucciarone@gmail.com, jcn23@psu.edu")
 
+-- Add Python related files
+prepend_path("PATH", "/gpfs/group/dml129/default/sw7/python-3.9.4/bin")
+prepend_path("LIBRARY_PATH", "/gpfs/group/dml129/default/sw7/python-3.9.4/lib")
+prepend_path("LD_LIBRARY_PATH", "/gpfs/group/dml129/default/sw7/python-3.9.4/lib")
+prepend_path("INCLUDE", "/gpfs/group/dml129/default/sw7/python-3.9.4/include")
+prepend_path("C_INCLUDE_PATH", "/gpfs/group/dml129/default/sw7/python-3.9.4/include")
+prepend_path("CPLUS_INCLUDE_PATH", "/gpfs/group/dml129/default/sw7/python-3.9.4/include")
+prepend_path("MANPATH", "/gpfs/group/dml129/default/sw7/python-3.9.4/share/man")
+prepend_path("PKG_CONFIG_PATH", "/gpfs/group/dml129/default/sw7/python-3.9.4/lib/pkgconfig")
+prepend_path("CMAKE_PREFIX_PATH", "/gpfs/group/dml129/default/sw7/python-3.9.4")
+
+-- Add iaskutils specifc paths
 prepend_path("PATH", "/gpfs/group/dml129/default/sw7/iaskutils/bin")
 prepend_path("MANPATH", "/gpfs/group/dml129/default/sw7/iaskutils/share/man")
